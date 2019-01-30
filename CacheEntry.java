a9562acb2b441b1b863099c6bd378de13cfa50e2
mccoleman75225 <michaelcoleman19@gmail.com>
2019-01-30 18:11:28 -0500

    commit after runnign code in eclipse

--- CacheEntry.java
+++ CacheEntry.java
@@ -1,13 +1,68 @@
-public class CacheEntry {
-  private String key;
-  private int value;
+import java.util.HashMap;
+import java.util.Map;
 
-  public CacheEntry(String key, int value) {
-    this.key = key;
-    this.value = value;
-  }
+ public class CacheEntry<K,V> {
+   private K key;
+   private V value;
+   private long currentTimeMillis;
+  
 
-  public static void main(String args[]) {
-    System.out.println("hello world");
-  }
-}
+   public CacheEntry(K key, V value) {
+     this.key = key;
+     this.value = value;
+     this.currentTimeMillis = System.currentTimeMillis();
+   }
+
+   public K getKey() {
+     return this.key;
+   }
+
+   public V getValue() {
+     return this.value;
+   }
+   
+   public long getLastUseTime() {
+	   return this.currentTimeMillis;
+   }
+
+   public void setValue(V value) {
+     this.value = value;
+     this.currentTimeMillis = System.currentTimeMillis();
+   }
+
+//   public K toString() {
+//     return "Key: " + this.key + " Value: "  + this.value.toString();
+//   }
+
+ }
+
+//public class CacheEntry<T, U> {
+//  private final Map<T, U> map = new HashMap<>();
+//
+//  public void put(T key, U value) {
+//    map.put(key, value);
+//  }
+//
+//  public <T> T getKey() {
+//    return (T) map.get(key);
+//  }
+//  
+//  public <T> T getValue() {
+//	    return (T) map.get(value);
+//	  }
+//
+// public static class Key<T> {
+//  private final String name;
+//
+//  public Key(String name) {
+//    this.name = name;
+//  }
+//  public String getName() {
+//    return name;
+//  }
+//
+//
+// }
+//
+//
+//}
--- SetAssociativeCache.java
+++ SetAssociativeCache.java
@@ -1,13 +1,92 @@
-public class SetAssociativeCache {
-  private final int numSets;
-  private final int sizeOfSet;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-  private CacheEntry[][] cacheEntries;
 
-  public SetAssociativeCache(int numSets, int sizeOfSet) {
+public class SetAssociativeCache<T, U> {
+	
+   private final int numSets;
+   private final int sizeOfSet;
+   private final ReplacementPolicy replacementPolicy;
+   public enum ReplacementPolicy { MRU, LRU };
+	
+  private CacheEntry<T,U>[][] cacheEntries;
+
+  public SetAssociativeCache(int numSets, int sizeOfSet, ReplacementPolicy replacementPolicy) {
     this.numSets = numSets;
     this.sizeOfSet = sizeOfSet;
     this.cacheEntries = new CacheEntry[numSets][sizeOfSet];
+    this.replacementPolicy = replacementPolicy;
   }
 
+  public final Object get(T key) {
+    int set = key.hashCode() % numSets;
+
+    for( CacheEntry entry : cacheEntries[set]) {
+      if( entry != null ) {
+        if( entry.getKey() == key) {
+          return entry.getValue();
+        }
+      }
+    }
+    return null;
+  }
+
+  public void put(T key, U value) {
+	  // what occurs if there's already an entry in the cacheEntry
+    int set = key.hashCode() % numSets;
+    for( CacheEntry entry : cacheEntries[set]) {
+      if( entry != null ) {
+        if( entry.getKey() == key) {
+          entry.setValue(value);
+          return;
+        }
+      }
+    }
+    // what occurs if there's not already an entry n the cacheEntry
+    CacheEntry cacheEntry = new CacheEntry<T, U>(key,value);
+    for( int i = 0; i < this.sizeOfSet; i++) {
+      if( cacheEntries[set][i] == null ) {
+        cacheEntries[set][i] = cacheEntry;
+        return;
+      }
+    }
+    
+    if(this.replacementPolicy == ReplacementPolicy.MRU) {
+    CacheEntry<T,U> minimumCacheEntry = cacheEntries[set][0];
+    int indexOfMin = 0;
+    for( int i = 1; i < this.sizeOfSet; i++) {
+        if( cacheEntries[set][i].getLastUseTime() < minimumCacheEntry.getLastUseTime() ) {
+        	minimumCacheEntry = cacheEntries[set][i];
+        	indexOfMin = i;
+        }
+      }
+    cacheEntries[set][indexOfMin] = cacheEntry;
+    
+    } else if (this.replacementPolicy == ReplacementPolicy.LRU) {
+    	CacheEntry<T,U> minimumCacheEntry = cacheEntries[set][0];
+        int indexOfMin = 0;
+        
+        for( int i = 1; i < this.sizeOfSet; i++) {
+            if( cacheEntries[set][i].getLastUseTime() < minimumCacheEntry.getLastUseTime() ) {
+            	minimumCacheEntry = cacheEntries[set][i];
+            	indexOfMin = i;
+            }
+    }
+    } else {
+    	// custom replacement policy goes here
+    }
+  }
+  
+  public String toString() {
+	  return Arrays.deepToString(cacheEntries);
+  }
+
+  public static void main(String args[]) {
+	  SetAssociativeCache cache = new SetAssociativeCache<String, Integer>(2,3,ReplacementPolicy.LRU);
+//	  cache.put("hey","hello!");
+//	  cache.put(1,1);
+	  cache.put("hello", 1);
+	  System.out.println(cache.get("hello"));
+  }
 }
